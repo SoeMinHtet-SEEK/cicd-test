64450f681d470fe7686c159a76673c139ba1ab34
shtet <shtet@seekasia.com>
2025-10-25 22:09:25 +0800

    Change Build to always success

--- build.sh
+++ build.sh
@@ -4,9 +4,4 @@ set -e  # stop if any command fails
 echo "🏗️ Starting CI build..."
 sleep 1
 
-if (( RANDOM % 2 )); then
-    echo "❌ Simulated build failure"
-    exit 1
-fi
-
 echo "✅ Build completed successfully"
